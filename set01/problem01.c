8e35c13581b10841f67f04c04dcefa2d369dcc4e
NehaDhami25 <nehadhami.bsc23@rvu.edu.in>
2024-01-29 10:56:10 +0530

    test

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,1 +1,7 @@
 // 1. Write a C program to print your name.
+#include <stdio.h>
+int main()
+{
+    printf("My name is Neha Dhami");
+    return 0;
+}
--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,10 @@
 //2. Write a C program to add two numbers.
-
+#include <stdio.h>
+int main()
+{
+    int a=3,b=5;
+    int add = 0;
+    add = a+b;
+    printf("the sum is %d", &add);
+    return 0;
+}
